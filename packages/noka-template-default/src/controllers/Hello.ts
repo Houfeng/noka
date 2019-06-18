e1af2dca9b27940082023628814c5fdf37b90f6b
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2019-06-18 19:46:21 +0800

    noka-template-default@1.0.3

--- packages/noka-template-default/src/controllers/Hello.ts
+++ packages/noka-template-default/src/controllers/Hello.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, Render } from "noka";
+import { Controller, Get, Inject, Render, Body, Post } from "noka";
 import { ItemService } from "../services/Hello";
 
 @Controller("/")
@@ -7,9 +7,11 @@ export class HelloController {
   itemService: ItemService;
 
   @Get("/")
+  @Post("/")
   @Render("index")
-  async index() {
-    await this.itemService.create();
+  async index(@Body("remove") removeId: string, @Body("create") add: string) {
+    if (add) await this.itemService.create();
+    if (removeId) await this.itemService.remove(removeId);
     const items = await this.itemService.list();
     return { items };
   }
