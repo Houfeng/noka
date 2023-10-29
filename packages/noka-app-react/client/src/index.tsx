8e0545900523ffce8c7f6eb4bc5656d5e3a6fb3a
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-10-29 21:42:01 +0800

    refine: 完善 React 应用模板

--- packages/noka-app-react/client/src/index.tsx
+++ packages/noka-app-react/client/src/index.tsx
@@ -1,9 +1,44 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createTheme,
+  ThemeProvider,
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const theme = createTheme({
+  palette: { mode: 'dark' }
+});
 
 function App() {
   return (
-    <div>你好中国</div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   )
 }
 
