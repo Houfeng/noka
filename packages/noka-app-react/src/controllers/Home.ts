c32fdab90e54094ad6d4bf964ec7e076cbe3334d
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-10-30 20:22:40 +0800

    refine: 优化 React 应用模板

--- packages/noka-app-react/src/controllers/Home.ts
+++ packages/noka-app-react/src/controllers/Home.ts
@@ -6,15 +6,20 @@ import { resolve } from "path";
 
 const { NOKA_ENV } = process.env;
 
-async function getAssets() {
+type Assets = { styles: string[], scripts: string[] };
+
+let cachedAssets: Assets;
+async function getAssets(): Promise<Assets> {
   const manifest = "pi-manifest.json";
   if (NOKA_ENV === "development") {
     const res = await fetch(`http://127.0.0.1:8081/${manifest}`);
     return res.json();
   } else {
+    if (cachedAssets) return cachedAssets;
     const file = resolve(__dirname, `../../assets/app/${manifest}`);
     const buffer = await readFile(file);
-    return JSON.parse(buffer.toString("utf-8"));
+    cachedAssets = JSON.parse(buffer.toString("utf-8"));
+    return cachedAssets;
   }
 }
 
